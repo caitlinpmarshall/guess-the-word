9cf2734475c50009633ef7c2708330aa1bdaae22
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-05-29 09:24:46 -0400

    Added function to display word in progress, replacing dots with correctly guessed letters. Not yet resolved: currently replaces only the first instance of a letter, not subsequent ones

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
 const guessedLetters = [];
 
-const placeholders = function(word) {
+const placeholders = function (word) {
     placeholderSymbols = [];
     for (let letter of word) {
         //console.log(letter);
@@ -38,15 +38,15 @@ placeholders(word);
 
 //can I put event listener at the bottom of the code, so it all flows in order?
 
-guessButton.addEventListener("click", function(e){
+guessButton.addEventListener("click", function (e) {
     e.preventDefault();
     userGuess = letterInput.value;
     //console.log(userGuess);
     letterInput.value = "";
     message.innerText = "";
     const validatedGuess = validateInput(userGuess); //I had "input" previously; why not input?
-    console.log(validatedGuess);
-    if (validatedGuess !== undefined){
+    console.log(`This is the acceptable letter you guessed: ${validatedGuess}`);
+    if (validatedGuess !== undefined) {
         makeGuess(validatedGuess);
     }
 });
@@ -63,11 +63,11 @@ const validateInput = function(input) {
 };
 */
 
-const validateInput = function(input) { //why not a function of userGuess?
+const validateInput = function (input) { //why not a function of userGuess?
     const acceptedLetter = /[a-zA-Z]/;
-    if (input === ""){
+    if (input === "") {
         message.innerText = "Oops, you left it blank.  Please enter a letter from A to Z."
-    } else if (input.length > 1){ 
+    } else if (input.length > 1) {
         message.innerText = "That's too many letters.  Please enter just one letter from A to Z."
     } else if (!input.match(acceptedLetter)) {
         message.innerText = "Letters only, please! No numbers or special characters."
@@ -77,22 +77,38 @@ const validateInput = function(input) { //why not a function of userGuess?
     }
 };
 
-const makeGuess = function(validatedGuess){ //"accepts a letter as the parameter" NB: and ONLY a letter
+const makeGuess = function (validatedGuess) { //"accepts a letter as the parameter" NB: and ONLY a letter
     //validatedGuess.toUpperCase(); toUpperCase is now appended to input inside the validator 
+    //validatedGuess was defined only within event listener, but I'm using it here; is that possible b/c makeGuess is called inside event listener?
     if (guessedLetters.includes(validatedGuess)) {
         message.innerText = "Oops, you've already guessed that one! Try another."
     } else {
         //guessedLetters.push(validatedGuess); used to have just this line of code
         showLetterGuessed(validatedGuess);
+        updateWordInProgress(guessedLetters);
     }
     console.log(guessedLetters);
 };
 
-const showLetterGuessed = function(validatedGuess){ // parameter = validatedGuess?
+const showLetterGuessed = function (validatedGuess) { // parameter = validatedGuess?
     guessedLettersElement.innerHTML = "";
     const li = document.createElement("li");
     li.innerText = `${validatedGuess}`;
     guessedLetters.push(li.innerText);
     guessedLettersElement.innerHTML = `${guessedLetters}`;
+};
+
+const updateWordInProgress = function (guessedLetters) {
+    //if word includes guessed letters, replace a dot with the correct letter
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const validatedGuess = validateInput(userGuess); // is it ok to define this again, inside this function?
+    console.log(`updateWordInProgress: These are the letters of the word to guess: ${wordArray}`);
+    for (let letter of wordArray) {
+        if (letter === validatedGuess){
+            placeholderSymbols.splice(wordArray.indexOf(letter),1,validatedGuess);
+            wordInProgress.innerText = `${placeholderSymbols.join("")}`;
+        }
+    }
 };
 
