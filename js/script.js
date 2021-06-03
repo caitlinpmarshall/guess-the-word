d0fe3c91bdf08124876c55aa7154e39b26c53479
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-06-03 14:15:31 -0400

    Fixes bugs that forced word variable back to magnolia, instead of random.  Game fully functioning now.  Display of letters guessed, from the showGuessedLetters function, is not ideal, since there is no space between the letters.

--- js/script.js
+++ js/script.js
@@ -18,12 +18,12 @@ let remainingGuesses = 8;
 
 // async to fetch a random word
 const getWord = async function () {
-    const randomWords = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
-    const randomWordsFormatted = await randomWords.text();
+    const response = await fetch ("https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt");
+    const randomWordsFormatted = await response.text();
     const randomWordsArray = randomWordsFormatted.split("\n");
     const randomIndex = Math.floor(Math.random()*randomWordsArray.length);
     const oneRandomWord = randomWordsArray[randomIndex].trim();
-    let word = oneRandomWord; //should this be let?
+    word = oneRandomWord; //should this be let? no! don't declare! because you already have. just reassigning here
     console.log(`value of variable word: ${word}`);
     placeholders(word);
 };
@@ -31,7 +31,7 @@ const getWord = async function () {
 // hide the word to guess as a series of dots
 const placeholders = function (word) {
     placeholderSymbols = [];
-    for (let letter of word) {
+    for (const letter of word) {
         //console.log(letter);
         placeholderSymbols.push("●");
     }
@@ -45,15 +45,17 @@ getWord();
 //user clicks the "guess" button to submit a letter
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     userGuess = letterInput.value;
     //console.log(userGuess);
-    letterInput.value = "";
-    message.innerText = "";
+    
+
     const validatedGuess = validateInput(userGuess); //I had "input" previously; why not input?
     console.log(`This is the acceptable letter you guessed: ${validatedGuess}`);
     if (validatedGuess !== undefined) {
         makeGuess(validatedGuess);
     }
+    letterInput.value = "";
 });
 
 // testing whether to use input or input.value, on a simpler function first
@@ -85,7 +87,7 @@ const validateInput = function (input) { //why not a function of userGuess?
 
 //once guess is validated as a letter, checks whether it's already been guessed. if not, runs functions to show the letter, tally remaining guesses, and update the word in progress
 const makeGuess = function (validatedGuess) { //"accepts a letter as the parameter" NB: and ONLY a letter
-    //validatedGuess.toUpperCase(); toUpperCase is now appended to input inside the validator 
+    //validatedGuess.toUpperCase(); //toUpperCase is now appended to input inside the validator 
     //validatedGuess was defined only within event listener, but I'm using it here; is that possible b/c makeGuess is called inside event listener?
     if (guessedLetters.includes(validatedGuess)) {
         message.innerText = "Oops, you've already guessed that one! Try another."
@@ -115,7 +117,7 @@ const updateWordInProgress = function (guessedLetters) {
     const revealWord = [];
     for (const letter of wordArray) {
         if (guessedLetters.includes(letter)) { // why don't need a for...of loop to cycle through guessedLetters?
-            revealWord.push(letter);
+            revealWord.push(letter.toUpperCase());
         } else {
             revealWord.push("●");
         }
@@ -127,19 +129,19 @@ const updateWordInProgress = function (guessedLetters) {
 };
 
 //tallies remaining guesses allowed
-const countGuessesRemaining = function(input) { //userGuess, input, or validatedGuess as the parameter? userGuess and validatedGuess aren't global, so I'm going with input
+const countGuessesRemaining = function(userGuess) { //userGuess, input, or validatedGuess as the parameter? userGuess and validatedGuess aren't global, so I'm going with input
     //tell user if their guess is in the word
     //might need to uppercase again here
-    word.toUpperCase();
-    if (!word.includes(input)) {
+    const upperWord = word.toUpperCase();
+    if (!upperWord.includes(userGuess)) {
         remainingGuesses -= 1;
         message.innerText = "Sorry! That's not a letter in the word.";
     } else {
-        message.innerText = `Good guess! ${input} is in the word.`;
+        message.innerText = `Good guess! ${userGuess} is in the word.`;
     }
     //update tally of remaining guesses
     if (remainingGuesses === 0) {
-        message.innerText = `Game over! The word was ${word}. Better luck next time!`;
+        message.innerText = `Game over! The word was "${word}". Better luck next time!`;
     } else if (remainingGuesses === 1) {
         remainingGuessesElement.innerText = "You have just one guess left!";
     } else {
@@ -152,7 +154,9 @@ const countGuessesRemaining = function(input) { //userGuess, input, or validated
 const checkWin = function(revealWord){ //why no parameter required?
     //confirm whether the word in progress matches final word
     //used stringify, since this situation truly requires just strings
-    if (word === JSON.stringify(revealWord)){
+    console.log(`checkWin word variable: ${word.toUpperCase()}`);
+    console.log(wordInProgress.innerText);
+    if (word.toUpperCase() === wordInProgress.innerText){
         message.classList.add("win");
         message.innerHTML = '<p class="highlight">You guessed correct the word! Congrats!</p>';
     }
